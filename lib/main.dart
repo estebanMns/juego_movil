079c5c5779b2378147c9021e0743ffbcfbb3e352
Evelin Pulsara <evelin.pulsara@campusucc.edu.co>
2026-04-21 17:16:37 -0500

    Merge resuelto + iconos actualizados

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,15 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import './src/app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await dotenv.load(fileName: ".env");
 
   await Supabase.initialize(
     url: dotenv.env['supabase_Url']!,
-    anonKey: dotenv.env['supabase_Key']!);
+    anonKey: dotenv.env['supabase_Key']!,
+  );
 
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
