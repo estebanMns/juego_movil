179083cb86e9534f9eae80b874c65d0f6c987390
estebanMns <esteban.meneses@campusucc.edu.co>
2026-04-21 10:37:22 -0500

    creando Scriptde autenticacion para supabase

---
+++ lib/service/auth_services.dart
@@ -0,0 +1,39 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthServices {
+
+  final SupabaseClient _client = Supabase.instance.client;
+
+  // metodo para el registro
+  Future<void> signUp(String email, String password) async {
+    final response = await _client.auth.signUp(
+      email: email,
+      password: password
+      );
+
+      if (response.user == null){
+        throw Exception("Error al registrarse intentelo de nuevo");
+      }
+  }
+
+  //metodo para el inicio de sesion
+  Future<void> signIn(String email, String password) async {
+    final response = await _client.auth.signInWithPassword(
+      email: email,
+      password: password
+    );
+    
+    if (response.user == null){
+      throw Exception("usuario o contraseña incorrectos intentelo de nuevo");
+    }
+  }
+
+  //metodo para cerrar sesion
+  Future<void> signOut() async {
+    await _client.auth.signOut();
+  }
+
+  //getter para el estado de la autenticacion
+  Stream<AuthState> get authState => _client.auth.onAuthStateChange;
+
+}
